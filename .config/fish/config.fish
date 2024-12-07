fb6ce4625b4b63ce732f3dba51a7369ea2fb2aec
erffy <ceyhunerenerguven.07@proton.me>
2024-12-07 14:44:24 +0300

    12072024.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,10 +10,4 @@ alias ls="eza"
 alias zd="z"
 alias y="yay"
 
-set -gx PNPM_HOME "/home/eren/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
