c461c57239db7acd0491a6a9af2076b15f597153
erffy <ceyhunerenerguven.07@proton.me>
2024-12-14 12:13:58 +0300

    Refactor memory formatting and parsing in Waybar
    - Introduced a new FmtSize struct to handle memory size formatting with appropriate units (G, M, K, B).
    - Updated the format function to utilize the new FmtSize struct for cleaner output.
    - Refactored the parse function to improve memory information extraction from /proc/meminfo, enhancing readability and maintainability.
    - Adjusted the main function to output memory information in a structured format, improving the display in Waybar.

--- .config/waybar/src/memory.zig
+++ .config/waybar/src/memory.zig
@@ -4,6 +4,31 @@ const KILO: u64 = 1024;
 const MEGA: u64 = KILO * KILO;
 const GIGA: u64 = KILO * MEGA;
 
+const FmtSize = struct {
+    size: u64,
+
+    pub fn format(self: FmtSize, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        const scaled_size = self.size * KILO;
+        return if (scaled_size >= GIGA) {
+            try std.fmt.formatType(@as(f64, @floatFromInt(scaled_size)) / GIGA, fmt, options, writer, 0);
+            try writer.writeByte('G');
+        } else if (scaled_size >= MEGA) {
+            try std.fmt.formatType(@as(f64, @floatFromInt(scaled_size)) / MEGA, fmt, options, writer, 0);
+            try writer.writeByte('M');
+        } else if (scaled_size >= KILO) {
+            try std.fmt.formatType(@as(f64, @floatFromInt(scaled_size)) / KILO, fmt, options, writer, 0);
+            try writer.writeByte('K');
+        } else {
+            try std.fmt.formatType(scaled_size, fmt, options, writer, 0);
+            try writer.writeByte('B');
+        };
+    }
+};
+
+fn fmtSize(size: u64) FmtSize {
+    return .{ .size = size };
+}
+
 const MemoryInfo = struct {
     mem_total: u64 = 0,
     mem_used: u64 = 0,
@@ -25,101 +50,97 @@ const MemoryInfo = struct {
     kernel_stack: u64 = 0,
     page_tables: u64 = 0,
     slab: u64 = 0,
+
+    pub fn format(mem_info: MemoryInfo, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        const total_usage = mem_info.mem_used + mem_info.swap_used;
+        const total_percentage = @as(f64, @floatFromInt(total_usage)) / @as(f64, @floatFromInt(mem_info.mem_total + mem_info.swap_total)) * 100;
+
+        try writer.print(
+            "{{\"text\":\"  {d:.2} · {d:.0}%\",\"tooltip\":\"Total · {d:.2}\\nUsed · {d:.2}\\nFree · {d:.2}\\nAvailable · {d:.2}\\nShared · {d:.2}\\nBuffer / Cache · {d:.2}\\n\\nActive · {d:.2}\\nInactive · {d:.2}\\nAnon Pages · {d:.2}\\nMapped · {d:.2}\\nDirty · {d:.2}\\nWriteback · {d:.2}\\nKernel Stack · {d:.2}\\nPage Tables · {d:.2}\\nSlab · {d:.2}\\n\\nSwap Total · {d:.2}\\nSwap Free · {d:.2}\\nSwap Used · {d:.2}\"}}",
+            .{
+                fmtSize(total_usage),
+                total_percentage,
+                fmtSize(mem_info.mem_total),
+                fmtSize(mem_info.mem_used),
+                fmtSize(mem_info.mem_free),
+                fmtSize(mem_info.mem_available),
+                fmtSize(mem_info.mem_shared),
+                fmtSize(mem_info.mem_buff_cache),
+                fmtSize(mem_info.active),
+                fmtSize(mem_info.inactive),
+                fmtSize(mem_info.anon_pages),
+                fmtSize(mem_info.mapped),
+                fmtSize(mem_info.dirty),
+                fmtSize(mem_info.writeback),
+                fmtSize(mem_info.kernel_stack),
+                fmtSize(mem_info.page_tables),
+                fmtSize(mem_info.slab),
+                fmtSize(mem_info.swap_total),
+                fmtSize(mem_info.swap_free),
+                fmtSize(mem_info.swap_used),
+            },
+        );
+    }
 };
 
-fn format(size: u64, output: []u8) []const u8 {
-    const scaled_size = size * KILO;
-
-    return if (scaled_size >= GIGA)
-        std.fmt.bufPrint(output, "{d:.2}G", .{@as(f64, @floatFromInt(scaled_size)) / GIGA}) catch ""
-    else if (scaled_size >= MEGA)
-        std.fmt.bufPrint(output, "{d:.2}M", .{@as(f64, @floatFromInt(scaled_size)) / MEGA}) catch ""
-    else if (scaled_size >= KILO)
-        std.fmt.bufPrint(output, "{d:.2}K", .{@as(f64, @floatFromInt(scaled_size)) / KILO}) catch ""
-    else
-        std.fmt.bufPrint(output, "{}B", .{scaled_size}) catch "";
-}
-
-fn parseLine(line: []const u8, prefix: []const u8) ?u64 {
-    if (!std.mem.startsWith(u8, line, prefix)) return null;
-
-    const value_start = std.mem.trimLeft(u8, line[prefix.len..], " ");
-    const end_index = std.mem.indexOfScalar(u8, value_start, ' ') orelse value_start.len;
-
-    return std.fmt.parseInt(u64, value_start[0..end_index], 10) catch null;
+fn asInt(s: []const u8) u128 {
+    var buf = [1]u8{0} ** 16;
+    if (s.len <= buf.len) @memcpy(buf[0..s.len], s);
+    return @bitCast(buf);
 }
 
 fn parse() !MemoryInfo {
     const file = try std.fs.cwd().openFile("/proc/meminfo", .{});
     defer file.close();
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
+    var br = std.io.bufferedReader(file.reader());
+    const reader = br.reader();
 
     var result = MemoryInfo{};
-    var line_buf: [256]u8 = undefined;
     var buffers: u64 = 0;
     var cached: u64 = 0;
 
-    while (try in_stream.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
-        if (parseLine(line, "MemTotal:")) |val| result.mem_total = val;
-        if (parseLine(line, "MemFree:")) |val| result.mem_free = val;
-        if (parseLine(line, "MemAvailable:")) |val| result.mem_available = val;
-        if (parseLine(line, "Buffers:")) |val| buffers = val;
-        if (parseLine(line, "Cached:")) |val| cached = val;
-        if (parseLine(line, "Shmem:")) |val| result.mem_shared = val;
-        if (parseLine(line, "SwapTotal:")) |val| result.swap_total = val;
-        if (parseLine(line, "SwapFree:")) |val| result.swap_free = val;
-
-        if (parseLine(line, "Active:")) |val| result.active = val;
-        if (parseLine(line, "Inactive:")) |val| result.inactive = val;
-        if (parseLine(line, "AnonPages:")) |val| result.anon_pages = val;
-        if (parseLine(line, "Mapped:")) |val| result.mapped = val;
-        if (parseLine(line, "Dirty:")) |val| result.dirty = val;
-        if (parseLine(line, "Writeback:")) |val| result.writeback = val;
-        if (parseLine(line, "KernelStack:")) |val| result.kernel_stack = val;
-        if (parseLine(line, "PageTables:")) |val| result.page_tables = val;
-        if (parseLine(line, "Slab:")) |val| result.slab = val;
+    while (true) {
+        var name_int: u128 = 0;
+        const name_buf = std.mem.asBytes(&name_int);
+        _ = (reader.readUntilDelimiterOrEof(name_buf, ':') catch name_buf) orelse break;
+        const ptr: ?*u64 = switch (name_int) {
+            asInt("MemTotal:") => &result.mem_total,
+            asInt("MemFree:") => &result.mem_free,
+            asInt("MemAvailable:") => &result.mem_available,
+            asInt("Buffers:") => &buffers,
+            asInt("Cached:") => &cached,
+            asInt("Shmem:") => &result.mem_shared,
+            asInt("SwapTotal:") => &result.swap_total,
+            asInt("SwapFree:") => &result.swap_free,
+            asInt("Active:") => &result.active,
+            asInt("Inactive:") => &result.inactive,
+            asInt("AnonPages:") => &result.anon_pages,
+            asInt("Mapped:") => &result.mapped,
+            asInt("Dirty:") => &result.dirty,
+            asInt("Writeback:") => &result.writeback,
+            asInt("KernelStack:") => &result.kernel_stack,
+            asInt("PageTables:") => &result.page_tables,
+            asInt("Slab:") => &result.slab,
+            else => null,
+        };
+        var line_buf: [256 - @sizeOf(@TypeOf(name_int))]u8 = undefined;
+        const line = reader.readUntilDelimiterOrEof(&line_buf, '\n') catch &line_buf orelse break;
+        if (ptr) |p| {
+            const value_start = std.mem.trimLeft(u8, line, " ");
+            const end_index = std.mem.indexOfScalar(u8, value_start, ' ') orelse value_start.len;
+            p.* = try std.fmt.parseUnsigned(u64, value_start[0..end_index], 10);
+        }
     }
 
     result.mem_buff_cache = buffers + cached;
     result.mem_used = result.mem_total - result.mem_available;
     result.swap_used = result.swap_total - result.swap_free;
-
     return result;
 }
 
 pub fn main() !void {
     const mem_info = try parse();
-
-    var mem_total_str: [16]u8 = undefined;
-    var mem_used_str: [16]u8 = undefined;
-    var mem_free_str: [16]u8 = undefined;
-    var mem_available_str: [16]u8 = undefined;
-    var mem_shared_str: [16]u8 = undefined;
-    var mem_cache_str: [16]u8 = undefined;
-
-    var swap_total_str: [16]u8 = undefined;
-    var swap_free_str: [16]u8 = undefined;
-    var swap_used_str: [16]u8 = undefined;
-
-    var active_str: [16]u8 = undefined;
-    var inactive_str: [16]u8 = undefined;
-    var anonpages_str: [16]u8 = undefined;
-    var mapped_str: [16]u8 = undefined;
-    var dirty_str: [16]u8 = undefined;
-    var writeback_str: [16]u8 = undefined;
-    var kernelstack_str: [16]u8 = undefined;
-    var pagetables_str: [16]u8 = undefined;
-    var slab_str: [16]u8 = undefined;
-
-    var total_usage_str: [16]u8 = undefined;
-
-    const total_usage = mem_info.mem_used + mem_info.swap_used;
-    const formatted_total_usage = format(total_usage, &total_usage_str);
-
-    const total_percentage = @as(f64, @floatFromInt(total_usage)) /
-        @as(f64, @floatFromInt(mem_info.mem_total + mem_info.swap_total)) * 100;
-
-    try std.io.getStdOut().writer().print("{{\"text\":\"  {s} · {d:.0}%\",\"tooltip\":\"Total · {s}\\nUsed · {s}\\nFree · {s}\\nAvailable · {s}\\nShared · {s}\\nBuffer / Cache · {s}\\n\\nActive · {s}\\nInactive · {s}\\nAnon Pages · {s}\\nMapped · {s}\\nDirty · {s}\\nWriteback · {s}\\nKernel Stack · {s}\\nPage Tables · {s}\\nSlab · {s}\\n\\nSwap Total · {s}\\nSwap Free · {s}\\nSwap Used · {s}\"}}", .{ formatted_total_usage, total_percentage, format(mem_info.mem_total, &mem_total_str), format(mem_info.mem_used, &mem_used_str), format(mem_info.mem_free, &mem_free_str), format(mem_info.mem_available, &mem_available_str), format(mem_info.mem_shared, &mem_shared_str), format(mem_info.mem_buff_cache, &mem_cache_str), format(mem_info.active, &active_str), format(mem_info.inactive, &inactive_str), format(mem_info.anon_pages, &anonpages_str), format(mem_info.mapped, &mapped_str), format(mem_info.dirty, &dirty_str), format(mem_info.writeback, &writeback_str), format(mem_info.kernel_stack, &kernelstack_str), format(mem_info.page_tables, &pagetables_str), format(mem_info.slab, &slab_str), format(mem_info.swap_total, &swap_total_str), format(mem_info.swap_free, &swap_free_str), format(mem_info.swap_used, &swap_used_str) });
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    try bw.writer().print("{}", .{mem_info});
+    try bw.flush();
 }
